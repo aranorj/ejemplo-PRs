32157a62cffa016da69f1523fcbb46f611fdcf98
aranorj <arano.rocio.b@gmail.com>
2024-03-01 20:52:58 -0300

    fix: slider tablet

--- holaMundo.js
+++ holaMundo.js
@@ -2,3 +2,12 @@ console.log("Commit inicial");
 console.log("Crear Develop");
 console.log("Footer");
 console.log("Slider Mobile");
+console.log("FIX: slider tablet");
+
+//SIEMPRE ANTES DE COMMIT ACTUALIZO CON RAMA MADRE 
+
+// GIT SWITCH RAMA-MADRE 
+//GIT PULL ORIGIN RAMA-MADRE 
+//GIT SWITCH RAMA-HIJA 
+// GIT MERGE RAMA-MADRE 
+
